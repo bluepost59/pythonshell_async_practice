3f287d1babedd4fe2600d01a3dd134193d0b9bfc
bluepost <email>
2021-11-17 19:33:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const { PythonShell } = require("python-shell");
+
+// PythonShell.runString(
+//     'import json; print(json.dumps({"hoge":3.14}))',
+//     { mode: "json" },
+//     (err, result) => { console.log(result[0].hoge); });
